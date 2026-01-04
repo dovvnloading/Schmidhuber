5baa9e974cf738a6616e2ff13520a6ee010c90a2
Matthew Wesney <157447210+dovvnloading@users.noreply.github.com>
2026-01-03 21:05:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-// --- START OF FILE vite.config.ts ---
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -7,9 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // ADD THIS LINE: Matches your repository name
-      base: "/Schmidhuber/",
-      
+      base: '/Schmidhuber/',
       server: {
         port: 3000,
         host: '0.0.0.0',
